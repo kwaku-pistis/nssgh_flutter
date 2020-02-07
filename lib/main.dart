4d08baad1402f6624d0fc4a108bd58020acbf935
Pistis <danielpartey.dp@gmail.com>
2020-02-07 15:09:35 +0000

    [IMP] main home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:nssgh/screens/main_drawer.dart';
 
@@ -10,20 +11,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NSS GH',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        primaryColor: Color(0xff006b3f),
-        accentColor: Color(0xffce1126),
-        primaryColorDark: Color(0xff004629)
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.green,
+          primaryColor: Color(0xff006b3f),
+          accentColor: Color(0xffce1126),
+          primaryColorDark: Color(0xff004629)),
       home: MyHomePage(title: 'Home'),
     );
   }
@@ -48,19 +48,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -76,36 +63,247 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/adinkra_pattern.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(bottom: 15.0),
+              child: CarouselSlider(
+                height: 200.0,
+                items: <Widget>[
+                  Image(
+                    image: AssetImage('assets/images/slider_one.png'),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/slider_two.png'),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/slider_three.png'),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                ],
+                aspectRatio: 16 / 9,
+                viewportFraction: 1.0,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 10),
+                autoPlayAnimationDuration: Duration(milliseconds: 1000),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                pauseAutoPlayOnTouch: Duration(seconds: 10),
+                enlargeCenterPage: true,
+                onPageChanged: null,
+                scrollDirection: Axis.horizontal,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Expanded(
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      GridView.count(
+                        padding: const EdgeInsets.fromLTRB(60.0, 0, 60.0, 0),
+                        crossAxisCount: 2,
+                        shrinkWrap: true,
+                        children: <Widget>[
+                          Container(
+                            color: Color(0xff4340ce),
+                            child: Image(
+                              image: AssetImage(
+                                  'assets/images/ic_online_registration.png'),
+                              width: 150.0,
+                              height: 150.0,
+                            ),
+                          ),
+                          Container(
+                            width: 150.0,
+                            height: 150.0,
+                            color: Color(0xff390158),
+                            child: Image(
+                              image: AssetImage(
+                                  'assets/images/ic_find_posting.png'),
+                            ),
+                          ),
+                          Container(
+                            width: 150.0,
+                            height: 150.0,
+                            color: Color(0xffff7400),
+                            child: Image(
+                              image: AssetImage(
+                                  'assets/images/ic_check_allowance.png'),
+                            ),
+                          ),
+                          Container(
+                            width: 150.0,
+                            height: 150.0,
+                            color: Color(0xfffb2700),
+                            child: Image(
+                              image:
+                                  AssetImage('assets/images/ic_check_pin.png'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 25, 25, 15),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 0.5,
+                        )
+                      ),
+                      Text(
+                        'NSS DOCUMENTS', 
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 60, right: 10),
+                            child: Icon(Icons.file_download, color: Color(0xffCE1126), size: 30,)
+                          ),
+                          Text(
+                              'A list of downloadable NSS documents',
+                              style: TextStyle(fontSize: 16),
+                            )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                      Text(
+                        'USEFUL LINKS', 
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 60, right: 10),
+                            child: Icon(Icons.language, color: Color(0xffCE1126), size: 30,)
+                          ),
+                          Text(
+                              'Ministry of Education',
+                              style: TextStyle(fontSize: 16),
+                            )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 60, right: 10),
+                            child: Icon(Icons.language, color: Color(0xffCE1126), size: 30,)
+                          ),
+                          Text(
+                              'GetFund Ghana',
+                              style: TextStyle(fontSize: 16),
+                            )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 60, right: 10),
+                            child: Icon(Icons.language, color: Color(0xffCE1126), size: 30,)
+                          ),
+                          Text(
+                              'GHIPSS',
+                              style: TextStyle(fontSize: 16),
+                            )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 60, right: 10),
+                            child: Icon(Icons.language, color: Color(0xffCE1126), size: 30,)
+                          ),
+                          Text(
+                              'Rules and Regulations',
+                              style: TextStyle(fontSize: 16),
+                            )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                        child: Image(
+                          image: AssetImage('assets/images/line.png'),
+                          height: 1,
+                        )
+                      ),
+                    ],
+                  )),
+            ),
+            Container(
+              height: 25.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/pattern_bar.png'),
+                      fit: BoxFit.cover)),
             ),
           ],
         ),
       ),
+
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
--- lib/screens/main_drawer.dart
+++ lib/screens/main_drawer.dart
@@ -5,205 +5,206 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        children: <Widget>[
-          // Container(
-          //   width: double.infinity,
-          //   padding: EdgeInsets.all(20.0),
-          //   color: Theme.of(context).primaryColor,
-          //   child: Center(
-          //     child: Column(
-          //       children: [
-          //         Container(
-          //           width: 100,
-          //           height: 100,
-          //           margin: EdgeInsets.only(
-          //             top: 60.0,
-          //             bottom: 10,
-          //           ),
-          //           decoration: BoxDecoration(
-          //             shape: BoxShape.circle,
-          //             image: DecorationImage(
-          //               image: NetworkImage(
-          //                   'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
-          //               fit: BoxFit.cover,
-          //             ),
-          //           ),
-          //         ),
-          //         Text(
-          //           'pistis@gmail.com',
-          //           style: TextStyle(
-          //             fontSize: 18.0,
-          //             color: Colors.white,
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          // ListTile(
-          //   leading: Icon(Icons.person),
-          //   title: Text(
-          //     'Profile',
-          //     style: TextStyle(fontSize: 18),
-          //   ),
-          //   onTap: () {
-          //     Navigator.of(context).pop();
-          //   },
-          // ),
-          // ListTile(
-          //   leading: Icon(Icons.person),
-          //   title: Text('Profile'),
-          //   onTap: null,
-          // ),
-          // ListTile(
-          //   leading: Icon(Icons.person),
-          //   title: Text('Profile'),
-          //   onTap: null,
-          // ),
-          // ListTile(
-          //   leading: Icon(Icons.person),
-          //   title: Text('Profile'),
-          //   onTap: null,
-          // ),
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            // Container(
+            //   width: double.infinity,
+            //   padding: EdgeInsets.all(20.0),
+            //   color: Theme.of(context).primaryColor,
+            //   child: Center(
+            //     child: Column(
+            //       children: [
+            //         Container(
+            //           width: 100,
+            //           height: 100,
+            //           margin: EdgeInsets.only(
+            //             top: 60.0,
+            //             bottom: 10,
+            //           ),
+            //           decoration: BoxDecoration(
+            //             shape: BoxShape.circle,
+            //             image: DecorationImage(
+            //               image: NetworkImage(
+            //                   'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+            //               fit: BoxFit.cover,
+            //             ),
+            //           ),
+            //         ),
+            //         Text(
+            //           'pistis@gmail.com',
+            //           style: TextStyle(
+            //             fontSize: 18.0,
+            //             color: Colors.white,
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            // ListTile(
+            //   leading: Icon(Icons.person),
+            //   title: Text(
+            //     'Profile',
+            //     style: TextStyle(fontSize: 18),
+            //   ),
+            //   onTap: () {
+            //     Navigator.of(context).pop();
+            //   },
+            // ),
+            // ListTile(
+            //   leading: Icon(Icons.person),
+            //   title: Text('Profile'),
+            //   onTap: null,
+            // ),
+            // ListTile(
+            //   leading: Icon(Icons.person),
+            //   title: Text('Profile'),
+            //   onTap: null,
+            // ),
+            // ListTile(
+            //   leading: Icon(Icons.person),
+            //   title: Text('Profile'),
+            //   onTap: null,
+            // ),
 
-          /// this is the main one I will be using
-          /// starting with the header
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10.0),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/adinkra_pattern.png'),
+            /// this is the main one I will be using
+            /// starting with the header
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10.0),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/adinkra_pattern.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    height: 60,
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.only(top: 25.0, bottom: 5.0),
+                    child: Image.asset(
+                      'assets/images/nss.png',
+                    ),
+                  ),
+                  Text(
+                    'Our mission is to mobilize and deploy Ghanaian citizens 18 years and above for national development',
+                    style: TextStyle(),
+                  ),
+                  // Container(
+                  //   width: double.infinity,
+                  //   height: 20.0,
+                  //   decoration: BoxDecoration(
+                  //     image: DecorationImage(
+                  //       image: AssetImage('assets/images/pattern_bar.png'),
+                  //       fit: BoxFit.fill
+                  //     )
+                  //   ),
+                  // )
+                ],
+              ),
+            ),
+            Container(
+              height: 25.0,
+              child: Image.asset(
+                'assets/images/pattern_bar.png',
                 fit: BoxFit.cover,
               ),
             ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: double.infinity,
-                  height: 60,
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(top: 25.0, bottom: 5.0),
-                  child: Image.asset(
-                    'assets/images/nss.png',
-                  ),
-                ),
-                Text(
-                  'Our mission is to mobilize and deploy Ghanaian citizens 18 years and above for national development',
-                  style: TextStyle(),
-                ),
-                // Container(
-                //   width: double.infinity,
-                //   height: 20.0,
-                //   decoration: BoxDecoration(
-                //     image: DecorationImage(
-                //       image: AssetImage('assets/images/pattern_bar.png'),
-                //       fit: BoxFit.fill
-                //     )
-                //   ),
-                // )
-              ],
+
+            // Main menu drawers
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: null,
             ),
-          ),
-          Container(
-            height: 30.0,
-            child: Image.asset(
-              'assets/images/pattern_bar.png',
-              fit: BoxFit.fill,
+            ListTile(
+              leading: Icon(Icons.language),
+              title: Text('NSS Portal'),
+              onTap: null,
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.receipt),
+              title: Text('Request Certificate'),
+              onTap: null,
+            ),
+            Container(
+              width: double.infinity,
+              child: Image.asset('assets/images/line.png'),
+            ),
 
-          // Main menu drawers
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: null,
-          ),
-          ListTile(
-            leading: Icon(Icons.language),
-            title: Text('NSS Portal'),
-            onTap: null,
-          ),
-          ListTile(
-            leading: Icon(Icons.receipt),
-            title: Text('Request Certificate'),
-            onTap: null,
-          ),
-          Container(
-            width: double.infinity,
-            child: Image.asset('assets/images/line.png'),
-          ),
+            // deployment menus
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(16.0),
+              child: Text('Deployment'),
+            ),
+            ListTile(
+              leading: Icon(Icons.location_on),
+              title: Text('Check Posting'),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.block),
+              title: Text('Exemptions'),
+              onTap: null,
+            ),
+            Container(
+              width: double.infinity,
+              child: Image.asset('assets/images/line.png'),
+            ),
 
-          // deployment menus
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(16.0),
-            child: Text('Deployment'),
-          ),
-          ListTile(
-            leading: Icon(Icons.location_on),
-            title: Text('Check Posting'),
-            onTap: null,
-          ),
-          ListTile(
-            leading: Icon(Icons.block),
-            title: Text('Exemptions'),
-            onTap: null,
-          ),
-          Container(
-            width: double.infinity,
-            child: Image.asset('assets/images/line.png'),
-          ),
+            // Projects menu
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(16.0),
+              child: Text('Projects'),
+            ),
+            ListTile(
+              leading: Icon(Icons.spa),
+              title: Text('Farms'),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.play_for_work),
+              title: Text('Borehole'),
+              onTap: null,
+            ),
+            Container(
+              width: double.infinity,
+              child: Image.asset('assets/images/line.png'),
+            ),
 
-          // Projects menu
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(16.0),
-            child: Text('Projects'),
-          ),
-          ListTile(
-            leading: Icon(Icons.spa),
-            title: Text('Farms'),
-            onTap: null,
-          ),
-          ListTile(
-            leading: Icon(Icons.play_for_work),
-            title: Text('Borehole'),
-            onTap: null,
-          ),
-          Container(
-            width: double.infinity,
-            child: Image.asset('assets/images/line.png'),
-          ),
-
-          // Media menus
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(16.0),
-            child: Text('Media'),
-          ),
-          ListTile(
-            leading: Icon(Icons.import_contacts),
-            title: Text('Blog'),
-            onTap: null,
-          ),
-          ListTile(
-            leading: Icon(Icons.video_library),
-            title: Text('Videos'),
-            onTap: null,
-          ),
-          ListTile(
-            leading: Icon(Icons.library_books),
-            title: Text('Press Releases'),
-            onTap: null,
-          ),
-        ],
-      ),
-    ));
+            // Media menus
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(16.0),
+              child: Text('Media'),
+            ),
+            ListTile(
+              leading: Icon(Icons.import_contacts),
+              title: Text('Blog'),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.video_library),
+              title: Text('Videos'),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.library_books),
+              title: Text('Press Releases'),
+              onTap: null,
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
