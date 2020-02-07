207605d4986b33c3872a1fc96d7544f6b9347d99
Pistis <danielpartey.dp@gmail.com>
2020-02-07 10:03:28 +0000

    [IMP] navigation drawer

--- lib/screens/main_drawer.dart
+++ lib/screens/main_drawer.dart
@@ -4,6 +4,8 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      child: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
       child: Column(
         children: <Widget>[
           // Container(
@@ -67,6 +69,7 @@ class MainDrawer extends StatelessWidget {
           // ),
 
           /// this is the main one I will be using
+          /// starting with the header
           Container(
             width: double.infinity,
             padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10.0),
@@ -89,8 +92,7 @@ class MainDrawer extends StatelessWidget {
                 ),
                 Text(
                   'Our mission is to mobilize and deploy Ghanaian citizens 18 years and above for national development',
-                  style: TextStyle(
-                  ),
+                  style: TextStyle(),
                 ),
                 // Container(
                 //   width: double.infinity,
@@ -111,9 +113,97 @@ class MainDrawer extends StatelessWidget {
               'assets/images/pattern_bar.png',
               fit: BoxFit.fill,
             ),
-          )
+          ),
+
+          // Main menu drawers
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.language),
+            title: Text('NSS Portal'),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.receipt),
+            title: Text('Request Certificate'),
+            onTap: null,
+          ),
+          Container(
+            width: double.infinity,
+            child: Image.asset('assets/images/line.png'),
+          ),
+
+          // deployment menus
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.all(16.0),
+            child: Text('Deployment'),
+          ),
+          ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text('Check Posting'),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.block),
+            title: Text('Exemptions'),
+            onTap: null,
+          ),
+          Container(
+            width: double.infinity,
+            child: Image.asset('assets/images/line.png'),
+          ),
+
+          // Projects menu
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.all(16.0),
+            child: Text('Projects'),
+          ),
+          ListTile(
+            leading: Icon(Icons.spa),
+            title: Text('Farms'),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.play_for_work),
+            title: Text('Borehole'),
+            onTap: null,
+          ),
+          Container(
+            width: double.infinity,
+            child: Image.asset('assets/images/line.png'),
+          ),
+
+          // Media menus
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.all(16.0),
+            child: Text('Media'),
+          ),
+          ListTile(
+            leading: Icon(Icons.import_contacts),
+            title: Text('Blog'),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.video_library),
+            title: Text('Videos'),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.library_books),
+            title: Text('Press Releases'),
+            onTap: null,
+          ),
         ],
       ),
-    );
+    ));
   }
 }
